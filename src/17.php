57b3029c4c18f2f1526e64baa53fde9bfbd4e834
waddillmaffeo473 <waddillmaffeo473@gmail.com>
2025-03-22 15:04:29 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,13 @@
+<?php
+// Your math problem here
+function solveMathProblem($problem) {
+    // Problem 1
+    echo "Solution to Problem 1: $problem\n";
+    
+    // Problem 2
+    echo "Solution to Problem 2: $problem\n";
+
+    // Problem 3
+    echo "Solution to Problem 3: $problem\n";
+}
+?>
