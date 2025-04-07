001efac50a299bcf642bb80cc394e59a5982e768
waddillmaffeo473 <waddillmaffeo473@gmail.com>
2025-04-07 17:31:59 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,7 @@
+<?php
+// Define the problem statement
+$problem = "Can you find the sum of all even numbers between 1 and 20?";
+
+// Print the PHP code to solve the problem
+echo $problem . "\n";
+?>
